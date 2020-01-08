76055ddb1cd09c27988018757e3a0f015518600b
joaquin <suavecali@yahoo.com>
2020-01-08 16:32:57 +0100

    move

--- ecr_basic/eks_cluster.tf
+++ ecr_basic/eks_cluster.tf
@@ -2,7 +2,8 @@ variable region {}
 variable eks_cluster_name {}
 
 module "eks-cluster-with-ecr" {
-  source           = "github.com/darkn3rd/eks-basic?ref=v0.0.2"
+  #source           = "github.com/darkn3rd/eks-basic?ref=v0.0.2"
+  source = "../eks-basic"
   region           = var.region
   eks_cluster_name = var.eks_cluster_name
 
